6e0c24ca436a4b0dbaf8aea884ba7a5c63dfe018
zagubygorilka <zagubygorilka@gamil.com>
2022-11-02 23:26:31 +0200

    Initial commit

---
+++ JS homeworks/lesson2/main.js
@@ -0,0 +1,88 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+// let arr = [10,"car", 3,123,true, false,"window"]
+//
+// console.log(arr)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let obj1 = {
+//     title:"bukvar",
+//     pageCount:12,
+//     gener:true
+// };
+// let obj2 = {
+//     title:"azbuka",
+//     pageCount:15,
+//     gener:true
+// }
+// let obj3 = {
+//     title:"kobzar",
+//     pageCount:354,
+//     gener:true
+// }
+// console.log(obj2);
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
+// Поле "автори" - масив. Кожен автор має поля name,age
+
+// let obj1 = {
+//     title:"bukvar",
+//     pageCount:12,
+//     gener:true,
+//     authors:[
+//         {
+//             name:"Taras Shevscenko",
+//             age:32
+//         },
+//         {
+//             name:"Lesya Ukrainka",
+//             age:27
+//         }
+//     ]
+// };
+// let obj2 = {
+//     title:"azbuka",
+//     pageCount:15,
+//     gener:true,
+//     authors: [
+//         {
+//             name:"Ivan Franko",
+//             age:22
+//         },
+//         {
+//           name:"Taras Stupka",
+//           age:20
+//         }
+//     ]
+// }
+// console.log(obj2);
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password.
+// Вивести в консоль пароль кожного користувача
+
+// let users = [
+//     {
+//         name:"Vasya",
+//         userName:"Vasyliv",
+//         password:12123123
+//     },
+//     {
+//         name:"Max",
+//         userName:"Senko",
+//         password:4754234
+//     },
+//     {
+//         name:"Petro",
+//         userName:"Diotre",
+//         password:83723428
+//     },
+//     {
+//         name:"Iryna",
+//         userName:"Pashko",
+//         password:38383
+//     }
+// ]
+//
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
