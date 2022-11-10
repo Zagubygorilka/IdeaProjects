b962c176737e0cc40685327d0db9f6a8f7ccf779
zagubygorilka <zagubygorilka@gamil.com>
2022-11-10 16:43:23 +0200

    !

--- JS homeworks/lesson4/main.js
+++ JS homeworks/lesson4/main.js
@@ -26,16 +26,13 @@
 // - Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
 // Масив:
-//
-//     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-//
-// for (const listOfItem of listOfItems) {
-//     document.write(`
-//             <ul>
-//                 <li>${listOfItem}</li>
-//             </ul>
-//     `)
-// }
+
+    let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+document.write(`<ul>`)
+for (const listOfItem of listOfItems){
+    document.write(`<li>${listOfItem}</li>`)
+}
+document.write(`</ul>`)
 
 
 //
